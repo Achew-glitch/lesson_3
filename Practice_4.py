416bfa2871a2cf8ad01209c54bb704fb2b093c5a
Achew <artem.kovzik@yandex.ru>
2020-11-12 14:08:28 +0300

    Выполнил практическое задание 4 первым способом

---
+++ Practice_4.py
@@ -0,0 +1,8 @@
+def my_func (arg_1, arg_2):
+    return arg_1 ** arg_2
+
+x = float(input('Введите действительное положительное число: '))
+y = int(input('Введите целое отрицательное число: '))
+
+print(my_func(x, y))
+
