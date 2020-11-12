b2daae223f012895b0470864f2c1d1994f6806bd
Achew <artem.kovzik@yandex.ru>
2020-11-12 14:21:09 +0300

    Выполнил практическое задание 4 вторым способом

--- Practice_4.py
+++ Practice_4.py
@@ -1,8 +1,11 @@
 def my_func (arg_1, arg_2):
-    return arg_1 ** arg_2
+    res = arg_1
+    for i in range(arg_2, 1):
+        res /= arg_1
+    return res
 
 x = float(input('Введите действительное положительное число: '))
 y = int(input('Введите целое отрицательное число: '))
 
 print(my_func(x, y))
-
+print(pow(x, y))
---
+++ main.py
@@ -0,0 +1,7 @@
+arg_1 = float(2)
+arg_2 = int(-1)
+
+for i in range(arg_2, 1):
+    arg_1 /= arg_1
+
+print(arg_1)
